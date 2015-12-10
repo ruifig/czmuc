f14f00377f0276eddf55e4cdc0a3095d7ca39769
Rui Figueira <ruimvfigueira@gmail.com>
2015-12-10 16:39:27 +0000

    refactor

--- source/crazygaze/ThreadingUtils.h
+++ source/crazygaze/ThreadingUtils.h
@@ -102,4 +102,23 @@ namespace cz
 
 	};
 
-} // namespace cz
+	template <class T>
+	class Monitor
+	{
+	private:
+		mutable T m_t;
+		mutable std::mutex m_mtx;
+
+	public:
+		using Type = T;
+		Monitor() {}
+		Monitor(T t_) : m_t(std::move(t_)) {}
+		template <typename F>
+		auto operator()(F f) const -> decltype(f(m_t))
+		{
+			std::lock_guard<std::mutex> hold{ m_mtx };
+			return f(m_t);
+		}
+	};
+	
+}  // namespace cz
