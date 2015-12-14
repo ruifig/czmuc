f20530eb16c6aececdc941a0314413c55825e099
ruifig <ruimvfigueira@gmail.com>
2015-12-14 23:40:52 +0000

    Added cz::clip

--- source/crazygaze/Algorithm.h
+++ source/crazygaze/Algorithm.h
@@ -95,4 +95,11 @@ namespace cz
 		return res;
 	}
 
+
+	template<typename T>
+	T clip(const T& n, const T& lower, const T& upper)
+	{
+		return std::max(lower, std::min(n, upper));
+	}
+
 } // namespace cz
