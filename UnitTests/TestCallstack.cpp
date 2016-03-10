44ff2f74e47244fc9ad61f1164e9ac718615c8a6
ruifig <ruimvfigueira@gmail.com>
2016-03-10 18:12:42 +0000

    Added Callstack::Iterator, so we can use range based for loops

--- UnitTests/TestCallstack.cpp
+++ UnitTests/TestCallstack.cpp
@@ -34,34 +34,41 @@ public:
 
 struct Bar
 {
-	int a;
+	Bar(std::string n = "") : name(n) {}
+	std::string name;
 };
 struct Foo
 {
-	int b;
+	Foo(std::string n = "") : name(n) {}
+	std::string name;
 };
 
 TEST(1)
 {
-	IOThread ioth(3);
 	Bar b1;
+	Bar b2;
 	Foo f1;
 
 	{
 		CHECK(Callstack<Bar>::contains(&b1) == nullptr);
+		CHECK(Callstack<Bar>::contains(&b2) == nullptr);
 		CHECK(Callstack<Foo>::contains(&f1) == nullptr);
 		{
 			Callstack<Bar>::Context ctx(&b1);
 			CHECK(Callstack<Bar>::contains(&b1) != nullptr);
+			CHECK(Callstack<Bar>::contains(&b2) == nullptr);
 			CHECK(Callstack<Foo>::contains(&f1) == nullptr);
 			{
 				Callstack<Bar>::Context ctx1(&b1);
 				Callstack<Foo>::Context ctx2(&f1);
+				Callstack<Bar>::Context ctx3(&b2);
 				CHECK(Callstack<Bar>::contains(&b1) != nullptr);
+				CHECK(Callstack<Bar>::contains(&b2) != nullptr);
 				CHECK(Callstack<Foo>::contains(&f1) != nullptr);
 			}
 		}
 		CHECK(Callstack<Bar>::contains(&b1) == nullptr);
+		CHECK(Callstack<Bar>::contains(&b2) == nullptr);
 		CHECK(Callstack<Foo>::contains(&f1) == nullptr);
 	}
 
@@ -90,6 +97,43 @@ TEST(1)
 		CHECK(BarT::contains(&b1) == nullptr);
 		CHECK(FooT::contains(&f1) == nullptr);
 	}
+}
+
+TEST(Iterators)
+{
+	Bar b1("b1");
+	Bar b2("b2");
+	Bar b3("b3");
+
+	using BarCallstack = Callstack<Bar, int>;
+
+	{
+		int v1 = 1;
+		BarCallstack::Context ctx(&b1, v1);
+		int v11 = 11;
+		BarCallstack::Context ctx2(&b1, v11);
+		{
+			int v2 = 2;
+			BarCallstack::Context ctx(&b2, v2);
+			{
+				int v3 = 3;
+				BarCallstack::Context ctx(&b3, v3);
+
+				std::vector<std::pair<Bar*, int>> v;
+				for (auto i : BarCallstack())
+					v.emplace_back(i->getKey(), *i->getValue());
+
+				CHECK_EQUAL(4, v.size());
+				CHECK(v[0].first == &b3 && v[0].second == 3);
+				CHECK(v[1].first == &b2 && v[1].second == 2);
+				CHECK(v[2].first == &b1 && v[2].second == 11);
+				CHECK(v[3].first == &b1 && v[3].second == 1);
+			}
+		}
+	}
+
+
+
 }
 
 }
--- source/crazygaze/Callstack.h
+++ source/crazygaze/Callstack.h
@@ -6,7 +6,8 @@
 	purpose:
 	Allows adding key:value pair markers to the current callstack.
 	This allows for example to check if an instance/function is already being executed
-	Very similar and inspired by boost::asio::detail::call_stack
+	Very similar and inspired by boost::asio::detail::call_stack,
+	with the addition that we can iterate the call stack with a range based for
 *********************************************************************/
 
 #pragma once
@@ -18,6 +19,8 @@ template<typename Key, typename Value = unsigned char>
 class Callstack
 {
 public:
+	class Iterator;
+
 	class Context
 	{
 	public:
@@ -43,11 +46,47 @@ public:
 		{
 			Callstack<Key,Value>::ms_top = m_next;
 		}
+
+		Key* getKey()
+		{
+			return m_key;
+		}
+
+		Value* getValue()
+		{
+			return m_val;
+		}
 	private:
 		friend class Callstack<Key, Value>;
+		friend class Callstack<Key, Value>::Iterator;
 		Key* m_key;
 		Value* m_val;
 		Context* m_next;
+	};
+
+	class Iterator
+	{
+	public:
+		Iterator(Context* ctx) : m_ctx(ctx) {}
+		Iterator& operator++()
+		{
+			if (m_ctx)
+				m_ctx = m_ctx->m_next;
+			return *this;
+		}
+
+		bool operator != (const Iterator& other)
+		{
+			return m_ctx != other.m_ctx;
+		}
+
+		Context* operator*()
+		{
+			return m_ctx;
+		}
+
+	private:
+		Context* m_ctx;
 	};
 
 	// Determine if the specified owner is on the stack
@@ -65,8 +104,17 @@ public:
 		return nullptr;
 	}
 
+	static Iterator begin()
+	{
+		return Iterator(ms_top);
+	}
+
+	static Iterator end()
+	{
+		return Iterator(nullptr);
+	}
+
 private:
-	friend class Context;
 	static thread_local Context* ms_top;
 };
 
